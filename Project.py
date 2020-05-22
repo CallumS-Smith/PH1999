ed376f712d08502733689d90d18f028ecef7acef
Callum Shingleton-Smith <callum@callum.com>
2020-05-22 13:47:14 +0100

    Testing log graph

--- Project.py
+++ Project.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 datapoints = 100
-x = np.arange(datapoints)
-y = x**3 - 100*x**2 + x + 1
+x = np.array([1,2,3,4,5,6])
+y = np.array([10,100,1000,10000,100000,1000000])
+y = np.log10(y)
 plt.plot(x,y)
-plt.xlabel("datapoints")
-plt.ylabel("sin(2$\pi$ datapoints)")
+plt.xlabel("Pressure/Pa")
+plt.ylabel("Volume/m$^3$")
+plt.axis([0,7,0,7])
 plt.show()

f600d08f77db633034db919c2d71f651339b7309
Callum Shingleton-Smith <callum@callum.com>
2020-06-08 17:14:36 +0100

    Final graphs with BEST line of best fit complete (Q13-14)

--- Project2.py
+++ Project2.py
@@ -6,7 +6,24 @@ yerror = np.array([0.05,0.05,0.05,0.05,0.05])
 #the next line is the model prediction,
 #where we make some initial guess
 #what the slope of the line will be
+total = 0
+for i in range(0,len(x)):
+    total += (y[i]-0.05*x[i])**2/(yerror[i])**2
+print(total)
+
+minchi2=10000
+minslope=0.0
+for slope in np.arange(0, 1, 0.0001):
+    total = 0
+    for i in range(0,len(x)):
+        total += (y[i]-slope*x[i])**2/(yerror[i])**2
+    if(total<minchi2):
+        minchi2=total
+        minslope=slope
+        
+print(minslope)
 fx=0.05*x
 plt.errorbar(x,y,yerror,fmt='bo')
 plt.plot(x,fx,'r-')
+plt.plot(x,minslope*x,'g-')
 plt.show()
